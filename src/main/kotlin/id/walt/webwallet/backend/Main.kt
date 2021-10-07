fd602434a0cccdf9f379c48057413529dab86fb0
Severin Stampler <severin@walt.id>
2021-10-07 16:01:06 +0200

    simple (mocked!) user management / login comment uncompilable / unused code

--- src/main/kotlin/id/walt/webwallet/backend/Main.kt
+++ src/main/kotlin/id/walt/webwallet/backend/Main.kt
@@ -1,8 +1,20 @@
 package id.walt.webwallet.backend
 
+import id.walt.webwallet.backend.auth.AuthController
+import id.walt.webwallet.backend.auth.JWTService
 import io.javalin.Javalin
+import io.javalin.apibuilder.ApiBuilder.*
+import javalinjwt.JWTAccessManager
 
 fun main(args: Array<String>) {
-    val app = Javalin.create().start(7000)
-    app.get("/") { ctx -> ctx.result("Hello World") }
+    val app = Javalin.create{ config ->
+        config.accessManager(JWTService)
+    }.start(8080)
+    app.before(JWTService.jwtHandler)
+
+    app.routes {
+        path("api") {
+            AuthController.routes
+        }
+    }
 }
---
+++ src/main/kotlin/id/walt/webwallet/backend/auth/UserRole.kt
@@ -0,0 +1,8 @@
+package id.walt.webwallet.backend.auth
+
+import io.javalin.core.security.RouteRole
+
+enum class UserRole : RouteRole {
+  UNAUTHORIZED,
+  AUTHORIZED
+}
