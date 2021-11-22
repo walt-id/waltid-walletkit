591b9c71b77fd7a4580f7a4972a9dfe2423113de
Severin Stampler <severin@walt.id>
2021-11-22 15:14:54 +0100

    support specifying data for issued credential

--- src/main/kotlin/id/walt/issuer/backend/IssuanceRequest.kt
+++ src/main/kotlin/id/walt/issuer/backend/IssuanceRequest.kt
@@ -3,5 +3,6 @@ package id.walt.issuer.backend
 data class IssuanceRequest (
   val user: String,
   val nonce: String,
-  val selectedCredentialIds: Set<String>
+  val selectedCredentialIds: Set<String>,
+  val params: Map<String, List<String>>
 )
