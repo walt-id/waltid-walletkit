629caec29a737bb54247f4a2fd2be31a70d23dea
Severin Stampler <severin@walt.id>
2021-12-09 16:52:40 +0100

    reenable issuance of id and diploma

--- src/main/kotlin/id/walt/issuer/backend/IssuerManager.kt
+++ src/main/kotlin/id/walt/issuer/backend/IssuerManager.kt
@@ -53,9 +53,9 @@ object IssuerManager {
 
   fun listIssuableCredentialsFor(user: String): List<IssuableCredential> {
     return listOf(
-//      IssuableCredential("1", "VerifiableId", "Verifiable ID document"),
-//      IssuableCredential("2", "VerifiableDiploma", "Verifiable diploma"),
-      IssuableCredential("1", "VerifiableVaccinationCertificate", "Verifiable vaccination certificate"),
+      IssuableCredential("1", "VerifiableId", "Verifiable ID document"),
+      IssuableCredential("2", "VerifiableDiploma", "Verifiable diploma"),
+      IssuableCredential("3", "VerifiableVaccinationCertificate", "Verifiable vaccination certificate"),
     )
   }
 
