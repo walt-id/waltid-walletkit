cd50b554c91e7b407bfb1ef27a57a24ee99c1c76
Philipp Potisk <phil@walt.id>
2022-01-26 15:14:17 +0100

    Returning VerifiablePresentation after verification step

--- src/main/kotlin/id/walt/verifier/backend/ResponseVerification.kt
+++ src/main/kotlin/id/walt/verifier/backend/ResponseVerification.kt
@@ -2,16 +2,17 @@ package id.walt.verifier.backend
 
 import id.walt.auditor.VerificationResult
 import id.walt.model.oidc.SIOPv2Request
+import id.walt.vclib.credentials.VerifiablePresentation
 
 data class ResponseVerification(
   val id: String,
   val subject: String?,
   val request: SIOPv2Request?,
-  val id_token: Boolean,
-  val verificationResult: VerificationResult?,
-  val vp_token: String?,
+  val id_token_valid: Boolean,
+  val verification_result: VerificationResult?,
+  val vp_token: VerifiablePresentation?,
   var auth_token: String?
 ) {
   val isValid
-    get() = !subject.isNullOrEmpty() && request != null && id_token && (verificationResult?.valid ?: false)
+    get() = !subject.isNullOrEmpty() && request != null && id_token_valid && (verification_result?.valid ?: false)
 }
--- src/main/kotlin/id/walt/verifier/backend/SIOPv2RequestManager.kt
+++ src/main/kotlin/id/walt/verifier/backend/SIOPv2RequestManager.kt
@@ -30,99 +30,99 @@ import java.util.*
 import java.util.concurrent.TimeUnit
 
 object SIOPv2RequestManager {
-  val reqCache = CacheBuilder.newBuilder().expireAfterWrite(5, TimeUnit.MINUTES).build<String, SIOPv2Request>()
-  val respCache = CacheBuilder.newBuilder().expireAfterWrite(5, TimeUnit.MINUTES).build<String, ResponseVerification>()
+    val reqCache = CacheBuilder.newBuilder().expireAfterWrite(5, TimeUnit.MINUTES).build<String, SIOPv2Request>()
+    val respCache =
+        CacheBuilder.newBuilder().expireAfterWrite(5, TimeUnit.MINUTES).build<String, ResponseVerification>()
 
-  val verifierContext = UserContext(
-    hkvStore = FileSystemHKVStore(FilesystemStoreConfig("$WALTID_DATA_ROOT/data/verifier")),
-    keyStore = HKVKeyStoreService(),
-    vcStore = HKVVcStoreService()
-  )
+    val verifierContext = UserContext(
+        hkvStore = FileSystemHKVStore(FilesystemStoreConfig("$WALTID_DATA_ROOT/data/verifier")),
+        keyStore = HKVKeyStoreService(),
+        vcStore = HKVVcStoreService()
+    )
 
-  fun newRequest(schemaUri: String): SIOPv2Request {
-    val nonce = UUID.randomUUID().toString()
-    val req = SIOPv2Request(
-      client_id = "${VerifierConfig.config.verifierApiUrl}/verify/$nonce",
-      redirect_uri = "${VerifierConfig.config.verifierApiUrl}/verify/$nonce",
-      response_mode = "form_post",
-      nonce = nonce,
-      registration = Registration(client_name = "Walt.id Verifier Portal", client_purpose = "Verification of ${Path.of(URL(schemaUri).path).fileName}"),
-      expiration = Instant.now().epochSecond + 24*60*60,
-      issuedAt = Instant.now().epochSecond,
-      claims = Claims(vp_token = VpTokenClaim(
-          presentation_definition = PresentationDefinition(
-            id = "1",
-            input_descriptors = listOf(
-              InputDescriptor(
-                id = "1",
-                schema = VpSchema(uri = schemaUri)
-              )
+    fun newRequest(schemaUri: String): SIOPv2Request {
+        val nonce = UUID.randomUUID().toString()
+        val req = SIOPv2Request(
+            client_id = "${VerifierConfig.config.verifierApiUrl}/verify/$nonce",
+            redirect_uri = "${VerifierConfig.config.verifierApiUrl}/verify/$nonce",
+            response_mode = "form_post",
+            nonce = nonce,
+            registration = Registration(
+                client_name = "Walt.id Verifier Portal",
+                client_purpose = "Verification of ${Path.of(URL(schemaUri).path).fileName}"
+            ),
+            expiration = Instant.now().epochSecond + 24 * 60 * 60,
+            issuedAt = Instant.now().epochSecond,
+            claims = Claims(
+                vp_token = VpTokenClaim(
+                    presentation_definition = PresentationDefinition(
+                        id = "1",
+                        input_descriptors = listOf(
+                            InputDescriptor(
+                                id = "1",
+                                schema = VpSchema(uri = schemaUri)
+                            )
+                        )
+                    )
+                )
             )
-          )
         )
-      )
-    )
-    reqCache.put(nonce, req)
-    return req
-  }
-
-  /*
-  - find cached request
-  - parse and verify id_token
-  - parse and verify vp_token
-  -  - compare nonce (verification policy)
-  -  - compare token_claim => token_ref => vp (verification policy)
-   */
-  fun verifyResponse(nonce: String, id_token: String, vp_token: String): ResponseVerification? {
-    val req = reqCache.getIfPresent(nonce)
-    if(req == null) {
-      return null
+        reqCache.put(nonce, req)
+        return req
     }
-    reqCache.invalidate(nonce)
-    val respId = UUID.randomUUID().toString()
-    val id_token_claims = JwtService.getService().parseClaims(id_token)!!
-    val sub = id_token_claims.get("sub").toString()
 
-    var result = ResponseVerification(
-      respId,
-      sub,
-      req,
-      ContextManager.runWith(verifierContext) {
-        if(!KeyService.getService().hasKey(sub))
-          DidService.importKey(sub)
-        JwtService.getService().verify(id_token)
-      },
-      ContextManager.runWith(verifierContext) {
-        when(vp_token.toCredential()) {
-          is VerifiablePresentation ->
-            Auditor.getService().verify(
-              vp_token, listOf(
-                SignaturePolicy(),
-                ChallengePolicy(req.nonce!!).apply {
-                  applyToVC = false },
-                VpTokenClaimPolicy(req.claims.vp_token)
-              )
-            )
-          else ->
+    /*
+    - find cached request
+    - parse and verify id_token
+    - parse and verify vp_token
+    -  - compare nonce (verification policy)
+    -  - compare token_claim => token_ref => vp (verification policy)
+     */
+    fun verifyResponse(nonce: String, id_token: String, vp_token: String): ResponseVerification? {
+        val req = reqCache.getIfPresent(nonce) ?: return null
+        reqCache.invalidate(nonce)
+        val respId = UUID.randomUUID().toString()
+        val id_token_claims = JwtService.getService().parseClaims(id_token)!!
+        val sub = id_token_claims.get("sub").toString()
+        val vp = vp_token.toCredential() as VerifiablePresentation
+
+        var result = ResponseVerification(
+            respId,
+            sub,
+            req,
+            ContextManager.runWith(verifierContext) {
+                if (!KeyService.getService().hasKey(sub))
+                    DidService.importKey(sub)
+                JwtService.getService().verify(id_token)
+            },
+            ContextManager.runWith(verifierContext) {
+
+                Auditor.getService().verify(
+                    vp_token, listOf(
+                        SignaturePolicy(),
+                        ChallengePolicy(req.nonce!!).apply {
+                            applyToVC = false
+                        },
+                        VpTokenClaimPolicy(req.claims.vp_token)
+                    )
+                )
+            },
+            vp_token = vp,
             null
+        )
+
+        if (result.isValid) {
+            result.auth_token = JWTService.toJWT(UserInfo(result.subject!!))
         }
-      },
-      vp_token = vp_token, // Return the VP to the frontend, so the data can be displayed
-      null
-    )
 
-    if (result.isValid) {
-      result.auth_token = JWTService.toJWT(UserInfo(result.subject!!))
+        respCache.put(result.id, result)
+
+        return result
     }
 
-    respCache.put(result.id, result)
-
-    return result
-  }
-
-  fun getVerificationResult(id: String): ResponseVerification? {
-    return respCache.getIfPresent(id).also {
-      respCache.invalidate(id)
+    fun getVerificationResult(id: String): ResponseVerification? {
+        return respCache.getIfPresent(id).also {
+            respCache.invalidate(id)
+        }
     }
-  }
 }
