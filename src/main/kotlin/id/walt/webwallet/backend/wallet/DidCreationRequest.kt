862ff2452a5e3179a7fac906f5ee530070f1db4d
Philipp Potisk <phil@walt.id>
2022-02-01 16:40:23 +0100

    Added params to DidCreationRequest

--- src/main/kotlin/id/walt/webwallet/backend/wallet/DidCreationRequest.kt
+++ src/main/kotlin/id/walt/webwallet/backend/wallet/DidCreationRequest.kt
@@ -3,5 +3,8 @@ package id.walt.webwallet.backend.wallet
 import com.beust.klaxon.Json
 
 data class DidCreationRequest(
-  @Json(serializeNull = false) val bearerToken: String? = null
+  @Json(serializeNull = false) val bearerToken: String? = null,
+  @Json(serializeNull = false) val didWebDomain: String? = null,
+  @Json(serializeNull = false) val didWebPath: String? = null,
+  @Json(serializeNull = false) val keyId: String? = null,
 )
