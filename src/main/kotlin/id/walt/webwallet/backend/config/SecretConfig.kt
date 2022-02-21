1b3f7d77bbf56e3d51c768663414d13845d056d4
Severin Stampler <severin@walt.id>
2022-02-21 11:22:00 +0100

    support loading oidc provider secrets from separate source (e.g. k8s secret)

---
+++ src/main/kotlin/id/walt/webwallet/backend/config/SecretConfig.kt
@@ -0,0 +1,15 @@
+package id.walt.webwallet.backend.config
+
+import id.walt.model.oidc.OIDCProvider
+
+data class SecretConfig (
+  val client_id: String,
+  val client_secret: String
+)
+
+data class SecretConfigMap(
+  val secrets: Map<String, SecretConfig>
+)
+
+fun OIDCProvider.withSecret(secretConfig: SecretConfig?): OIDCProvider =
+  OIDCProvider(this.id, this.url, this.description, secretConfig?.client_id ?: this.client_id, secretConfig?.client_secret ?: this.client_secret)
