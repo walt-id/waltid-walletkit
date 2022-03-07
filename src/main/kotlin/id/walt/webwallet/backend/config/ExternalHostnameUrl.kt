9d0d9aa01d12ad83201918c17a88bccd6087df6d
Severin Stampler <severin@walt.id>
2022-03-07 11:24:33 +0100

    fix replacing external hostname, if hostname variable is not set

--- src/main/kotlin/id/walt/webwallet/backend/config/ExternalHostnameUrl.kt
+++ src/main/kotlin/id/walt/webwallet/backend/config/ExternalHostnameUrl.kt
@@ -5,11 +5,26 @@ import com.beust.klaxon.Json
 import com.beust.klaxon.JsonValue
 import com.beust.klaxon.Klaxon
 import id.walt.model.oidc.OIDCProvider
+import java.io.File
+import java.nio.charset.StandardCharsets
 
 @Target(AnnotationTarget.FIELD)
 annotation class ExternalHostnameUrl
 
-fun replaceExternalHostname(url: String): String = url.replace("\$EXTERNAL_HOSTNAME", System.getenv("EXTERNAL_HOSTNAME") ?: System.getenv("HOSTNAME"))
+fun getExternalHostname() : String? {
+  return System.getenv("EXTERNAL_HOSTNAME")
+    ?: System.getenv("HOSTNAMEE") // linux
+    ?: File("/etc/hostname").let { file -> // linux alternative
+      if(file.exists()) {
+        file.readText(StandardCharsets.UTF_8).trim()
+      } else {
+        null
+      }
+    }
+    ?: System.getenv("COMPUTERNAME") // windows
+}
+
+fun replaceExternalHostname(url: String): String = getExternalHostname()?.let { url.replace("\$EXTERNAL_HOSTNAME", it) } ?: url
 
 val externalHostnameUrlValueConverter = object: Converter {
   override fun canConvert(cls: Class<*>) = cls == String::class.java
