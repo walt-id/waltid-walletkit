dc531242d5bcd37caafb8026440a902a3dcb6387
Severin Stampler <severin@walt.id>
2022-04-11 13:57:34 +0200

    hash user ids on storage (file system) #39, #33

--- src/main/kotlin/id/walt/webwallet/backend/context/UserContextLoader.kt
+++ src/main/kotlin/id/walt/webwallet/backend/context/UserContextLoader.kt
@@ -7,10 +7,19 @@ import id.walt.services.hkvstore.FilesystemStoreConfig
 import id.walt.services.keystore.HKVKeyStoreService
 import id.walt.services.vcstore.HKVVcStoreService
 import id.walt.WALTID_DATA_ROOT
+import java.security.MessageDigest
 
 object UserContextLoader : CacheLoader<String, Context>() {
+
+  private fun hashString(input: String): String {
+    return MessageDigest
+      .getInstance("SHA-256")
+      .digest(input.toByteArray())
+      .fold("", { str, it -> str + "%02x".format(it) })
+  }
+
   override fun load(key: String): UserContext {
     //TODO: get user context preferences from user database
-    return UserContext(key, HKVKeyStoreService(), HKVVcStoreService(), FileSystemHKVStore(FilesystemStoreConfig("${WALTID_DATA_ROOT}/data/${key}")))
+    return UserContext(key, HKVKeyStoreService(), HKVVcStoreService(), FileSystemHKVStore(FilesystemStoreConfig("${WALTID_DATA_ROOT}/data/${hashString(key)}")))
   }
 }
