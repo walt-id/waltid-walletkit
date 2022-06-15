425756ef98de6c962337fa67cffc178835141263
Philipp Potisk <phil@walt.id>
2022-06-15 15:20:38 +0200

    Added OpenBadgeCredential

--- src/main/kotlin/id/walt/issuer/backend/IssuerManager.kt
+++ src/main/kotlin/id/walt/issuer/backend/IssuerManager.kt
@@ -56,7 +56,7 @@ object IssuerManager {
 
   fun listIssuableCredentialsFor(user: String): Issuables {
     return Issuables(
-      credentials = listOf("VerifiableId", "VerifiableDiploma", "VerifiableVaccinationCertificate", "ProofOfResidence", "ParticipantCredential", "Europass")
+      credentials = listOf("VerifiableId", "VerifiableDiploma", "VerifiableVaccinationCertificate", "ProofOfResidence", "ParticipantCredential", "Europass", "OpenBadgeCredential")
         .map { IssuableCredential.fromTemplateId(it) }
     )
   }
