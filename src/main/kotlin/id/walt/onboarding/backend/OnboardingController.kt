88ee7182376951ababe898a239ee80247292a2ce
KBurgmann <kb@gatgetron.net>
2022-09-27 01:42:03 +0200

    Gaiax and add proofPurpose for Compliance API

--- src/main/kotlin/id/walt/onboarding/backend/OnboardingController.kt
+++ src/main/kotlin/id/walt/onboarding/backend/OnboardingController.kt
@@ -9,10 +9,15 @@ import id.walt.auditor.Auditor
 import id.walt.auditor.ChallengePolicy
 import id.walt.auditor.ChallengePolicyArg
 import id.walt.auditor.SignaturePolicy
-import id.walt.issuer.backend.*
+import id.walt.issuer.backend.IssuableCredential
+import id.walt.issuer.backend.Issuables
+import id.walt.issuer.backend.IssuerConfig
+import id.walt.issuer.backend.IssuerManager
 import id.walt.model.DidMethod
 import id.walt.model.DidUrl
-import id.walt.model.dif.*
+import id.walt.model.dif.CredentialManifest
+import id.walt.model.dif.OutputDescriptor
+import id.walt.model.dif.PresentationDefinition
 import id.walt.services.context.ContextManager
 import id.walt.services.jwt.JwtService
 import id.walt.services.oidc.OIDCUtils
@@ -61,7 +66,7 @@ object OnboardingController {
                 }
                 // provide customized oidc discovery document and authorize endpoint
                 path("oidc") {
-                   get(".well-known/openid-configuration", documented(
+                    get(".well-known/openid-configuration", documented(
                         document().operation {
                             it.summary("get OIDC provider meta data")
                                 .addTagsItem("Onboarding")
@@ -72,19 +77,30 @@ object OnboardingController {
                     ))
                     get("fulfillPAR", documented(
                         document().operation {
-                            it.summary("fulfill PAR").addTagsItem("Onboarding").operationId("fulfillPAR") }
+                            it.summary("fulfill PAR").addTagsItem("Onboarding").operationId("fulfillPAR")
+                        }
                             .queryParam<String>("request_uri"),
                         OnboardingController::fulfillPAR
                     ))
                 }
                 path("auth") {
-                    get("userToken", documented(document().operation {
-                        it.summary("get user token").addTagsItem("Onboarding").operationId("userToken") }.json<UserInfo>("200"),
-                        OnboardingController::userToken))
+                    get(
+                        "userToken", documented(
+                            document().operation {
+                                it.summary("get user token").addTagsItem("Onboarding").operationId("userToken")
+                            }.json<UserInfo>("200"),
+                            OnboardingController::userToken
+                        )
+                    )
                 }
-                post("issue", documented(document().operation {
-                    it.summary("Issue participant credential to did:web-authorized user").addTagsItem("Onboarding") }
-                    .queryParam<String>("sessionId").body<IssueParticipantCredentialRequest>(), OnboardingController::issue), UserRole.AUTHORIZED)
+                post(
+                    "issue",
+                    documented(document().operation {
+                        it.summary("Issue participant credential to did:web-authorized user").addTagsItem("Onboarding")
+                    }
+                        .queryParam<String>("sessionId").body<IssueParticipantCredentialRequest>(),
+                        OnboardingController::issue),
+                    UserRole.AUTHORIZED)
             }
 
     private fun generateDomainVerificationCode(ctx: Context) {
@@ -112,7 +128,8 @@ object OnboardingController {
         return userInfo.did!!
     }
 
-    const val PARICIPANT_CREDENTIAL_SCHEMA_ID = "https://raw.githubusercontent.com/walt-id/waltid-ssikit-vclib/master/src/test/resources/schemas/ParticipantCredential.json"
+    const val PARICIPANT_CREDENTIAL_SCHEMA_ID =
+        "https://raw.githubusercontent.com/walt-id/waltid-ssikit-vclib/master/src/test/resources/schemas/ParticipantCredential.json"
 
     private fun oidcProviderMeta(ctx: Context) {
         ctx.json(OIDCProviderMetadata(
@@ -123,21 +140,30 @@ object OnboardingController {
             authorizationEndpointURI = URI("${IssuerConfig.config.onboardingApiUrl}/oidc/fulfillPAR")
             pushedAuthorizationRequestEndpointURI = URI("${IssuerConfig.config.issuerApiUrl}/oidc/par") // keep issuer-api
             tokenEndpointURI = URI("${IssuerConfig.config.issuerApiUrl}/oidc/token") // keep issuer-api
-            setCustomParameter("credential_endpoint", "${IssuerConfig.config.issuerApiUrl}/oidc/credential") // keep issuer-api
+            setCustomParameter(
+                "credential_endpoint",
+                "${IssuerConfig.config.issuerApiUrl}/oidc/credential"
+            ) // keep issuer-api
             setCustomParameter("nonce_endpoint", "${IssuerConfig.config.issuerApiUrl}/oidc/nonce") // keep issuer-api
             setCustomParameter("credential_manifests", listOf(
                 CredentialManifest(
                     issuer = id.walt.model.dif.Issuer(IssuerManager.issuerDid, IssuerConfig.config.issuerClientName),
                     outputDescriptors = listOf(
                         OutputDescriptor(
-                                    "ParticipantCredential",
-                                PARICIPANT_CREDENTIAL_SCHEMA_ID,
-                                "ParticipantCredential")
+                            "ParticipantCredential",
+                            PARICIPANT_CREDENTIAL_SCHEMA_ID,
+                            "ParticipantCredential"
+                        )
                     ),
-                    presentationDefinition = PresentationDefinition("1", listOf()) // Request empty presentation to be sent along with issuance request
-                )).map { net.minidev.json.parser.JSONParser().parse(Klaxon().toJsonString(it)) }
+                    presentationDefinition = PresentationDefinition(
+                        "1",
+                        listOf()
+                    ) // Request empty presentation to be sent along with issuance request
+                )
+            ).map { net.minidev.json.parser.JSONParser().parse(Klaxon().toJsonString(it)) }
             )
-        }.toJSONObject())
+        }.toJSONObject()
+        )
     }
 
     fun fulfillPAR(ctx: Context) {
@@ -153,29 +179,36 @@ object OnboardingController {
                     ?: throw BadRequestResponse("No participant credential claim found in authorization request")
             val vp_token =
                 credClaim.vp_token
-                ?: session.authRequest.customParameters["vp_token"]?.flatMap {
-                    OIDCUtils.fromVpToken(it) ?: listOf()
-                } ?: listOf()
+                    ?: session.authRequest.customParameters["vp_token"]?.flatMap {
+                        OIDCUtils.fromVpToken(it) ?: listOf()
+                    } ?: listOf()
             val vp = vp_token.firstOrNull() ?: throw BadRequestResponse("No VP token found on authorization request")
             val subject = ContextManager.runWith(IssuerManager.issuerContext) {
-                val verificationResult = Auditor.getService().verify(vp.encode(), listOf(SignaturePolicy(), ChallengePolicy(ChallengePolicyArg(IssuerManager.getValidNonces()))))
-                if(!verificationResult.valid) {
+                val verificationResult = Auditor.getService().verify(
+                    vp.encode(),
+                    listOf(SignaturePolicy(), ChallengePolicy(ChallengePolicyArg(IssuerManager.getValidNonces())))
+                )
+                if (!verificationResult.valid) {
                     throw BadRequestResponse("Invalid VP token given, signature (${verificationResult.policyResults["SignaturePolicy"]}) and/or challenge (${verificationResult.policyResults["ChallengePolicy"]}) could not be verified")
                 }
                 vp.subject
             }
 
-            if(subject?.let { DidUrl.from(it).method } != DidMethod.web.name) throw BadRequestResponse("did:web is required for onboarding!")
+            if (subject?.let { DidUrl.from(it).method } != DidMethod.web.name) throw BadRequestResponse("did:web is required for onboarding!")
 
             session.did = subject
             IssuerManager.updateIssuanceSession(session, session.issuables)
 
             val access_token = ContextManager.runWith(IssuerManager.issuerContext) {
-                JwtService.getService().sign(IssuerManager.issuerDid, JWTClaimsSet.Builder().subject(session.id).build().toString())
+                JwtService.getService()
+                    .sign(IssuerManager.issuerDid, JWTClaimsSet.Builder().subject(session.id).build().toString())
             }
 
             ctx.status(HttpCode.FOUND)
-                .header("Location", "${IssuerConfig.config.onboardingUiUrl}?access_token=${access_token}&sessionId=${session.id}")
+                .header(
+                    "Location",
+                    "${IssuerConfig.config.onboardingUiUrl}?access_token=${access_token}&sessionId=${session.id}"
+                )
         } catch (exc: Exception) {
             ctx.status(HttpCode.FOUND).header(
                 "Location",
@@ -185,15 +218,17 @@ object OnboardingController {
     }
 
     fun userToken(ctx: Context) {
-        val accessToken = ctx.header("Authorization")?.let { it.substringAfterLast("Bearer ") } ?: throw UnauthorizedResponse("No valid access token set on request")
+        val accessToken = ctx.header("Authorization")?.let { it.substringAfterLast("Bearer ") }
+            ?: throw UnauthorizedResponse("No valid access token set on request")
         val sessionId = ContextManager.runWith(IssuerManager.issuerContext) {
-            if(JwtService.getService().verify(accessToken)) {
+            if (JwtService.getService().verify(accessToken)) {
                 JwtService.getService().parseClaims(accessToken)!!["sub"].toString()
             } else {
                 null
             }
         }
-        val session = sessionId?.let{ IssuerManager.getIssuanceSession(it) } ?: throw UnauthorizedResponse("Invalid access token or session expired")
+        val session = sessionId?.let { IssuerManager.getIssuanceSession(it) }
+            ?: throw UnauthorizedResponse("Invalid access token or session expired")
         val did = session.did ?: throw ForbiddenResponse("No DID specified on current session")
 
         val userInfo = UserInfo(did)
@@ -202,11 +237,12 @@ object OnboardingController {
 
     fun issue(ctx: Context) {
         val userInfo = JWTService.getUserInfo(ctx) ?: throw UnauthorizedResponse()
-        if(userInfo.did?.let { DidUrl.from(it).method } != DidMethod.web.name) {
+        if (userInfo.did?.let { DidUrl.from(it).method } != DidMethod.web.name) {
             throw BadRequestResponse("User is not did:web-authorized")
         }
-        val session = ctx.queryParam("sessionId")?.let{ IssuerManager.getIssuanceSession(it) } ?: throw BadRequestResponse("Session expired or not found")
-        if(userInfo.did != session.did) {
+        val session = ctx.queryParam("sessionId")?.let { IssuerManager.getIssuanceSession(it) }
+            ?: throw BadRequestResponse("Session expired or not found")
+        if (userInfo.did != session.did) {
             throw BadRequestResponse("Session DID not matching authorized DID")
         }
         // Use the following if we should rely on the domain used in the did:web
@@ -225,6 +261,13 @@ object OnboardingController {
                 )
             )
         )
-        ctx.result("${session!!.authRequest.redirectionURI}?code=${IssuerManager.updateIssuanceSession(session, selectedIssuables)}&state=${session.authRequest.state.value}")
+        ctx.result(
+            "${session.authRequest.redirectionURI}?code=${
+                IssuerManager.updateIssuanceSession(
+                    session,
+                    selectedIssuables
+                )
+            }&state=${session.authRequest.state.value}"
+        )
     }
 }
--- src/main/kotlin/id/walt/webwallet/backend/wallet/CredentialPresentation.kt
+++ src/main/kotlin/id/walt/webwallet/backend/wallet/CredentialPresentation.kt
@@ -19,138 +19,149 @@ import id.walt.vclib.templates.VcTemplateManager
 import id.walt.webwallet.backend.auth.UserInfo
 import java.time.Duration
 import java.util.*
-import java.util.concurrent.TimeUnit
+import java.util.concurrent.*
 
-data class CredentialPresentationSessionInfo (
-  val id: String,
-  val presentationDefinition: PresentationDefinition,
-  val isPassiveIssuanceSession: Boolean,
-  val redirectUri: String,
-  var did: String? = null,
-  var presentableCredentials: List<PresentableCredential>? = null,
-  var availableIssuers: List<OIDCProvider>? = null
-        )
+data class CredentialPresentationSessionInfo(
+    val id: String,
+    val presentationDefinition: PresentationDefinition,
+    val isPassiveIssuanceSession: Boolean,
+    val redirectUri: String,
+    var did: String? = null,
+    var presentableCredentials: List<PresentableCredential>? = null,
+    var availableIssuers: List<OIDCProvider>? = null
+)
 
-data class CredentialPresentationSession (
-  val id: String,
-  @Json(ignored = true) val req: AuthorizationRequest,
-  val sessionInfo: CredentialPresentationSessionInfo
-  )
+data class CredentialPresentationSession(
+    val id: String,
+    @Json(ignored = true) val req: AuthorizationRequest,
+    val sessionInfo: CredentialPresentationSessionInfo
+)
 
 data class PresentableCredential(
-  val credentialId: String,
-  val claimId: String?
+    val credentialId: String,
+    val claimId: String?
 )
 
 data class PresentationResponse(
-  val vp_token: String,
-  val presentation_submission: String,
-  val id_token: String?,
-  val state: String?
+    val vp_token: String,
+    val presentation_submission: String,
+    val id_token: String?,
+    val state: String?
 ) {
-  companion object {
-    fun fromSiopResponse(siopResp: SIOPv2Response): PresentationResponse {
-      return PresentationResponse(
-        OIDCUtils.toVpToken(siopResp.vp_token),
-        klaxon.toJsonString(siopResp.presentation_submission),
-        siopResp.id_token,
-        siopResp.state
-      )
+    companion object {
+        fun fromSiopResponse(siopResp: SIOPv2Response): PresentationResponse {
+            return PresentationResponse(
+                OIDCUtils.toVpToken(siopResp.vp_token),
+                klaxon.toJsonString(siopResp.presentation_submission),
+                siopResp.id_token,
+                siopResp.state
+            )
+        }
     }
-  }
 }
 
 object CredentialPresentationManager {
-  val EXPIRATION_TIME = Duration.ofMinutes(5)
-  val sessionCache = CacheBuilder.newBuilder().expireAfterAccess(EXPIRATION_TIME.seconds, TimeUnit.SECONDS).build<String, CredentialPresentationSession>()
+    val EXPIRATION_TIME = Duration.ofMinutes(5)
+    val sessionCache = CacheBuilder.newBuilder().expireAfterAccess(EXPIRATION_TIME.seconds, TimeUnit.SECONDS)
+        .build<String, CredentialPresentationSession>()
 
-  fun initCredentialPresentation(siopReq: AuthorizationRequest, passiveIssuance: Boolean): CredentialPresentationSession {
-    val id = UUID.randomUUID().toString()
-    return CredentialPresentationSession(
-      id = id,
-      req = siopReq,
-      sessionInfo = CredentialPresentationSessionInfo (
-        id,
-        presentationDefinition = OIDC4VPService.getPresentationDefinition(siopReq),
-        isPassiveIssuanceSession = passiveIssuance,
-        redirectUri = siopReq.redirectionURI.toString()
-      )
-    ).also {
-      sessionCache.put(it.id, it)
-    }
-  }
-
-  private fun getPresentableCredentials(session: CredentialPresentationSession): List<PresentableCredential> {
-    return OIDCUtils.findCredentialsFor(session.sessionInfo.presentationDefinition, session.sessionInfo.did).flatMap { kv ->
-        kv.value.map { credId -> PresentableCredential(credId, kv.key) }
-      }.toList()
-  }
-
-  private fun getRequiredSchemaIds(input_descriptors: List<InputDescriptor>): Set<String> {
-    return VcTemplateManager.getTemplateList().map { tmplId -> VcTemplateManager.loadTemplate(tmplId) }
-      .filter { templ -> input_descriptors.any { indesc -> OIDCUtils.matchesInputDescriptor(templ, indesc) } }
-      .map { templ -> templ.credentialSchema?.id }
-      .filterNotNull()
-      .toSet()
-  }
-
-  fun continueCredentialPresentationFor(sessionId: String, did: String): CredentialPresentationSession {
-    val session = sessionCache.getIfPresent(sessionId) ?: throw Exception("No session found for id $sessionId")
-    session.sessionInfo.did = did
-    session.sessionInfo.presentableCredentials = getPresentableCredentials(session)
-    session.sessionInfo.availableIssuers = null
-    if(session.sessionInfo.presentableCredentials!!.isEmpty()) {
-      val requiredSchemaIds = session.sessionInfo.presentationDefinition?.input_descriptors?.let { getRequiredSchemaIds(it) } ?: setOf()
-      if(requiredSchemaIds.isNotEmpty()) {
-        // credentials are required, but no suitable ones are found
-        session.sessionInfo.availableIssuers = CredentialIssuanceManager.findIssuersFor(requiredSchemaIds)
-      }
+    fun initCredentialPresentation(siopReq: AuthorizationRequest, passiveIssuance: Boolean): CredentialPresentationSession {
+        val id = UUID.randomUUID().toString()
+        return CredentialPresentationSession(
+            id = id,
+            req = siopReq,
+            sessionInfo = CredentialPresentationSessionInfo(
+                id,
+                presentationDefinition = OIDC4VPService.getPresentationDefinition(siopReq),
+                isPassiveIssuanceSession = passiveIssuance,
+                redirectUri = siopReq.redirectionURI.toString()
+            )
+        ).also {
+            sessionCache.put(it.id, it)
+        }
     }
 
-    return session
-  }
-
-  fun fulfillPresentation(sessionId: String, selectedCredentials: List<PresentableCredential>): SIOPv2Response {
-    val session = sessionCache.getIfPresent(sessionId) ?: throw Exception("No session found for id $sessionId")
-    val did = session.sessionInfo.did ?: throw  Exception("Did not set for this session")
-
-    val myCredentials = Custodian.getService().listCredentials()
-    val selectedCredentialIds = selectedCredentials.map { cred -> cred.credentialId }.toSet()
-    val selectedCredentials =
-      myCredentials.filter { cred -> selectedCredentialIds.contains(cred.id) }.map { cred -> cred.encode() }
-        .toList()
-    val vp = Custodian.getService().createPresentation(
-      selectedCredentials,
-      did,
-      null,
-      challenge = session.req.getCustomParameter("nonce")?.firstOrNull(),
-      expirationDate = null
-    ).toCredential() as VerifiablePresentation
-
-    val siopResponse = OIDC4VPService.getSIOPResponseFor(session.req, did, listOf(vp))
-
-    return siopResponse
-  }
-
-  fun fulfillPassiveIssuance(sessionId: String, selectedCredentials: List<PresentableCredential>, userInfo: UserInfo): CredentialIssuanceSession {
-    val session = sessionCache.getIfPresent(sessionId) ?: throw Exception("No session found for id $sessionId")
-    val siopResponse = fulfillPresentation(sessionId, selectedCredentials)
-
-    val body = OIDC4VPService.postSIOPResponse(session.req, siopResponse)
-    val credentials = Klaxon().parseArray<VerifiableCredential>(body)?.also { creds ->
-      creds.forEach { cred ->
-        Custodian.getService().storeCredential(cred.id!!, cred)
-      }
+    private fun getPresentableCredentials(session: CredentialPresentationSession): List<PresentableCredential> {
+        return OIDCUtils.findCredentialsFor(session.sessionInfo.presentationDefinition, session.sessionInfo.did).flatMap { kv ->
+            kv.value.map { credId -> PresentableCredential(credId, kv.key) }
+        }.toList()
     }
 
-    return CredentialIssuanceSession(
-      UUID.randomUUID().toString(),
-      issuanceRequest = CredentialIssuanceRequest(did = session.sessionInfo.did!!, issuerId = "", schemaIds = listOf(), walletRedirectUri = ""),
-      nonce = session.req.getCustomParameter("nonce")?.firstOrNull() ?: "",
-      user = userInfo,
-      credentials = credentials
-    ).also {
-      CredentialIssuanceManager.putSession(it)
+    private fun getRequiredSchemaIds(input_descriptors: List<InputDescriptor>): Set<String> {
+        return VcTemplateManager.getTemplateList().map { tmplId -> VcTemplateManager.loadTemplate(tmplId) }
+            .filter { templ -> input_descriptors.any { indesc -> OIDCUtils.matchesInputDescriptor(templ, indesc) } }
+            .map { templ -> templ.credentialSchema?.id }
+            .filterNotNull()
+            .toSet()
     }
-  }
+
+    fun continueCredentialPresentationFor(sessionId: String, did: String): CredentialPresentationSession {
+        val session = sessionCache.getIfPresent(sessionId) ?: throw Exception("No session found for id $sessionId")
+        session.sessionInfo.did = did
+        session.sessionInfo.presentableCredentials = getPresentableCredentials(session)
+        session.sessionInfo.availableIssuers = null
+        if (session.sessionInfo.presentableCredentials!!.isEmpty()) {
+            val requiredSchemaIds =
+                session.sessionInfo.presentationDefinition.input_descriptors.let { getRequiredSchemaIds(it) } ?: setOf()
+            if (requiredSchemaIds.isNotEmpty()) {
+                // credentials are required, but no suitable ones are found
+                session.sessionInfo.availableIssuers = CredentialIssuanceManager.findIssuersFor(requiredSchemaIds)
+            }
+        }
+
+        return session
+    }
+
+    fun fulfillPresentation(sessionId: String, selectedCredentials: List<PresentableCredential>): SIOPv2Response {
+        val session = sessionCache.getIfPresent(sessionId) ?: throw Exception("No session found for id $sessionId")
+        val did = session.sessionInfo.did ?: throw Exception("Did not set for this session")
+
+        val myCredentials = Custodian.getService().listCredentials()
+        val selectedCredentialIds = selectedCredentials.map { cred -> cred.credentialId }.toSet()
+        val selectedCredentials =
+            myCredentials.filter { cred -> selectedCredentialIds.contains(cred.id) }.map { cred -> cred.encode() }
+                .toList()
+        val vp = Custodian.getService().createPresentation(
+            selectedCredentials,
+            did,
+            null,
+            challenge = session.req.getCustomParameter("nonce")?.firstOrNull(),
+            expirationDate = null
+        ).toCredential() as VerifiablePresentation
+
+        val siopResponse = OIDC4VPService.getSIOPResponseFor(session.req, did, listOf(vp))
+
+        return siopResponse
+    }
+
+    fun fulfillPassiveIssuance(
+        sessionId: String,
+        selectedCredentials: List<PresentableCredential>,
+        userInfo: UserInfo
+    ): CredentialIssuanceSession {
+        val session = sessionCache.getIfPresent(sessionId) ?: throw Exception("No session found for id $sessionId")
+        val siopResponse = fulfillPresentation(sessionId, selectedCredentials)
+
+        val body = OIDC4VPService.postSIOPResponse(session.req, siopResponse)
+        val credentials = Klaxon().parseArray<VerifiableCredential>(body)?.also { creds ->
+            creds.forEach { cred ->
+                Custodian.getService().storeCredential(cred.id!!, cred)
+            }
+        }
+
+        return CredentialIssuanceSession(
+            UUID.randomUUID().toString(),
+            issuanceRequest = CredentialIssuanceRequest(
+                did = session.sessionInfo.did!!,
+                issuerId = "",
+                schemaIds = listOf(),
+                walletRedirectUri = ""
+            ),
+            nonce = session.req.getCustomParameter("nonce")?.firstOrNull() ?: "",
+            user = userInfo,
+            credentials = credentials
+        ).also {
+            CredentialIssuanceManager.putSession(it)
+        }
+    }
 }
