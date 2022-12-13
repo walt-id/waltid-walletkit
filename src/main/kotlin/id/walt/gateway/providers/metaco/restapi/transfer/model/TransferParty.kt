f4fbd3b1c8502cb203be7e1d3c5021d941341e40
mikeplotean <mike@walt.id>
2022-12-13 14:23:11 +0200

    transactions list (restapi use case)

---
+++ src/main/kotlin/id/walt/gateway/providers/metaco/restapi/transfer/model/TransferParty.kt
@@ -0,0 +1,20 @@
+package id.walt.gateway.providers.metaco.restapi.transfer.model
+
+import com.beust.klaxon.TypeAdapter
+import com.beust.klaxon.TypeFor
+import kotlinx.serialization.Serializable
+import kotlin.reflect.KClass
+
+@Serializable
+@TypeFor(field = "type", adapter = TransferPartyAdapter::class)
+sealed class TransferParty(
+    val type: String
+)
+
+class TransferPartyAdapter : TypeAdapter<TransferParty> {
+    override fun classFor(type: Any): KClass<out TransferParty> = when (type as String) {
+        "Address" -> AddressTransferParty::class
+        "Account" -> AccountTransferParty::class
+        else -> throw IllegalArgumentException("Unknown type: $type")
+    }
+}
