8d7a3e9bdbac2ff5b6859841b840861124ded12b
mikeplotean <mike@walt.id>
2022-12-14 13:49:54 +0200

    fixed serialization issue for ktor client

--- src/main/kotlin/id/walt/gateway/providers/metaco/restapi/intent/IntentRepositoryImpl.kt
+++ src/main/kotlin/id/walt/gateway/providers/metaco/restapi/intent/IntentRepositoryImpl.kt
@@ -3,12 +3,12 @@ package id.walt.gateway.providers.metaco.restapi.intent
 import com.beust.klaxon.Klaxon
 import id.walt.gateway.CommonHttp
 import id.walt.gateway.providers.metaco.repositories.IntentRepository
-import id.walt.gateway.providers.metaco.restapi.services.AuthService
 import id.walt.gateway.providers.metaco.restapi.BaseRestRepository
 import id.walt.gateway.providers.metaco.restapi.intent.model.SignatureIntent
 import id.walt.gateway.providers.metaco.restapi.intent.model.ValidationResponse
 import id.walt.gateway.providers.metaco.restapi.intent.model.payload.Payload
 import id.walt.gateway.providers.metaco.restapi.intent.model.result.IntentResult
+import id.walt.gateway.providers.metaco.restapi.services.AuthService
 
 class IntentRepositoryImpl(
     override val authService: AuthService,
