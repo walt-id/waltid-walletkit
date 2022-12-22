8df5127742d827b867da59e91fb161320c8405ae
mikeplotean <mike@walt.id>
2022-12-22 14:16:52 +0200

    hotfix: metaco private key

--- src/main/kotlin/id/walt/gateway/providers/metaco/ProviderConfig.kt
+++ src/main/kotlin/id/walt/gateway/providers/metaco/ProviderConfig.kt
@@ -1,5 +1,7 @@
 package id.walt.gateway.providers.metaco
 
+import java.io.File
+
 object ProviderConfig {
     val gatewayUrl: String = System.getenv("GATEWAY_URL") ?: ""
     val oauthUrl: String = System.getenv("OAUTH_URL") ?: ""
@@ -8,7 +10,8 @@ object ProviderConfig {
     val userId: String = System.getenv("USER_ID") ?: ""
     val domainId: String = System.getenv("DOMAIN_ID") ?: ""
     val publicKey: String = System.getenv("PUBLIC_KEY") ?: ""
-    val privateKey: String = System.getenv("PRIVATE_KEY") ?: ""
     val grantType: String = System.getenv("GRANT_TYPE") ?: ""
     val signServiceUrl: String = System.getenv("SIGN_SERVICE_URL") ?: ""
+    private val privateKeyPath: String = System.getenv("PRIVATE_KEY_PATH") ?: ""
+    val privateKey: String = File(privateKeyPath).takeIf { it.exists() }?.readText() ?: ""
 }
