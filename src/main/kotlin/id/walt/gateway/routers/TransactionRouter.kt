f525537a6c2704dfce0c1215e4bae2789547aaf2
mikeplotean <mike@walt.id>
2023-01-06 17:44:56 +0200

    * refactored provider routers and controllers * added provider mock api endpoint

--- src/main/kotlin/id/walt/gateway/routers/TransactionRouter.kt
+++ src/main/kotlin/id/walt/gateway/routers/TransactionRouter.kt
@@ -4,13 +4,15 @@ import id.walt.gateway.controllers.TradeController
 import io.javalin.apibuilder.ApiBuilder
 import io.javalin.plugin.openapi.dsl.documented
 
-object TransactionRouter: Router {
+class TransactionRouter(
+    private val tradeController: TradeController,
+) : Router {
     override fun routes() {
         ApiBuilder.path("trades") {
-            ApiBuilder.post("sell", documented(TradeController.sellDocs(), TradeController::sell))
-            ApiBuilder.post("buy", documented(TradeController.buyDocs(), TradeController::buy))
-            ApiBuilder.post("send", documented(TradeController.sendDocs(), TradeController::send))
-            ApiBuilder.post("validate", documented(TradeController.validateDocs(), TradeController::validate))
+            ApiBuilder.post("sell", documented(tradeController.sellDocs(), tradeController::sell))
+            ApiBuilder.post("buy", documented(tradeController.buyDocs(), tradeController::buy))
+            ApiBuilder.post("send", documented(tradeController.sendDocs(), tradeController::send))
+            ApiBuilder.post("validate", documented(tradeController.validateDocs(), tradeController::validate))
         }
     }
 }
