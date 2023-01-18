c4af3c1d342195cb364a126b5b2c4b260fed69f6
Severin Stampler <severin@walt.id>
2023-01-18 18:51:58 +0100

    enable using presentation definition by reference via verifier api, shorten uuid string representation

--- src/main/kotlin/id/walt/verifier/backend/VerifierState.kt
+++ src/main/kotlin/id/walt/verifier/backend/VerifierState.kt
@@ -3,6 +3,7 @@ package id.walt.verifier.backend
 import com.google.common.cache.CacheBuilder
 import com.nimbusds.oauth2.sdk.AuthorizationRequest
 import id.walt.issuer.backend.IssuerConfig
+import id.walt.model.dif.PresentationDefinition
 import id.walt.multitenancy.TenantState
 import java.util.concurrent.TimeUnit
 
@@ -10,6 +11,7 @@ class VerifierState: TenantState<VerifierConfig> {
   val reqCache = CacheBuilder.newBuilder().expireAfterWrite(5, TimeUnit.MINUTES).build<String, AuthorizationRequest>()
   val respCache =
     CacheBuilder.newBuilder().expireAfterWrite(5, TimeUnit.MINUTES).build<String, SIOPResponseVerificationResult>()
+  val presentationDefinitionCache = CacheBuilder.newBuilder().expireAfterWrite(5, TimeUnit.MINUTES).build<String, PresentationDefinition>()
 
   override var config: VerifierConfig? = null
 }
