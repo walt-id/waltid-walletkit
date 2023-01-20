684e0a5da53ca7cc07de669b964eba83b67054c9
mikeplotean <mike@walt.id>
2023-01-20 19:56:46 +0200

    cleanup

--- src/main/kotlin/id/walt/gateway/dto/intents/IntentBuilderParam.kt
+++
@@ -1,6 +0,0 @@
-package id.walt.gateway.dto.intents
-
-data class IntentBuilderParam(
-    val payloadType: String,
-    val parameterType: String,
-)
--- src/main/kotlin/id/walt/gateway/dto/intents/IntentValidationParameter.kt
+++
@@ -1,5 +0,0 @@
-package id.walt.gateway.dto.intents
-
-data class IntentValidationParameter(
-    val domainId: String,
-)
