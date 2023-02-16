d0a93eb2bd769317a146cf9b2fd169ab07dca03a
mikeplotean <mike@walt.id>
2023-02-16 20:08:34 +0200

    other: added intent expiry config

--- src/main/kotlin/id/walt/gateway/dto/intents/IntentParameter.kt
+++ src/main/kotlin/id/walt/gateway/dto/intents/IntentParameter.kt
@@ -1,13 +1,10 @@
 package id.walt.gateway.dto.intents
 
 import id.walt.gateway.dto.users.UserIdentifier
-import java.time.Duration
-import java.time.Instant
-import java.time.temporal.ChronoUnit
 
 data class IntentParameter(
     val targetDomainId: String,
     val author: UserIdentifier,
     val type: String? = null,
-    val expiry: Instant = Instant.now().plus(Duration.ofHours(1)).truncatedTo(ChronoUnit.SECONDS),
+    val expiry: String,
 )
