4537a6906f453238f5eeeccc494f9838e890e1d4
mikeplotean <mike@walt.id>
2023-02-16 19:34:15 +0200

    other: read coin mapping from external config

--- src/main/kotlin/id/walt/gateway/providers/metaco/CoinMapper.kt
+++ src/main/kotlin/id/walt/gateway/providers/metaco/CoinMapper.kt
@@ -2,22 +2,19 @@ package id.walt.gateway.providers.metaco
 
 import id.walt.gateway.dto.coins.CoinParameter
 import id.walt.gateway.providers.metaco.restapi.ticker.model.Ticker
+import java.io.File
 
 object CoinMapper {
+    val map: Map<String, String> by lazy { loadMap(ProviderConfig.coinMapPath) }
     fun Ticker.map(currency: String): CoinParameter = mapNameToCoinParameter(this.data.name, currency)
 
-    fun mapNameToCoinParameter(name: String, currency: String) = when (name.lowercase()) {
-        "bitcoin" -> "bitcoin"
-        "ethereum" -> "ethereum"
-        "polygon" -> "matic-network"
-        "polygon testnet mumbai" -> "polygon testnet mumbai"
-        "avalanche coin" -> "avalanche-2"
-        "dexalot token" -> "dexalot"
-        "bitcoin testnet" -> "t-bitcoin"
-        "tezos" -> "tezos"
-        "euro test 1" -> "euro"
-        "gold test 1" -> "gold"
-        else -> ""
-//            else -> throw IllegalArgumentException("Missing coin mapping for ticker name.")
-    }.let { CoinParameter(it, currency) }
+    fun mapNameToCoinParameter(name: String, currency: String) = CoinParameter(map[name.lowercase()] ?: "", currency)
+
+    private fun loadMap(filepath: String): Map<String, String> = File(filepath).takeIf { it.exists() }?.let {
+        it.readLines().associate {
+            it.split(",").takeIf { it.size > 1 }?.let {
+                Pair(it[0].lowercase(), it[1])
+            } ?: Pair("", "")
+        }.filter { it.key.isNotEmpty() }
+    } ?: emptyMap()
 }
