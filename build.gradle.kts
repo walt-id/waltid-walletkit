9ed59108393391f58c44462f2f39b9ec43230c55
Severin Stampler <severin@walt.id>
2023-02-27 16:16:36 +0100

    update ssikit to latest release

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,7 +21,7 @@ repositories {
 
 dependencies {
     // SSIKIT
-    implementation("id.walt:waltid-ssi-kit:1.13.0-SNAPSHOT9")
+    implementation("id.walt:waltid-ssikit:1.2302271506.0")
     //implementation("id.walt:waltid-ssikit-vclib:1.24.2")
 
     // Metaco
