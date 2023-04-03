223bb1c0e5ddf1f3ea8cced69778eabe197d9a7d
aminbenmansour <amin.benmansour.10@gmail.com>
2023-04-03 15:33:05 +0100

    polkadot account verification upon sign-in

--- src/main/kotlin/id/walt/webwallet/backend/auth/UserInfo.kt
+++ src/main/kotlin/id/walt/webwallet/backend/auth/UserInfo.kt
@@ -1,6 +1,7 @@
 package id.walt.webwallet.backend.auth
 
 import kotlinx.serialization.Serializable
+import org.bitcoinj.core.Base58
 
 @Serializable
 class UserInfo(
@@ -12,13 +13,28 @@ class UserInfo(
     var ethAccount: String? = null
     var did: String? = null
     var tezosAccount: String? = null
+    var polkadotAccount: String? = null
 
+    fun isPolkadotAccount(address: String): Boolean {
+        // Convert the SS58 address to its binary representation
+        val binaryAddress = Base58.decodeChecked(address).joinToString("") { byte ->
+            String.format("%8s", Integer.toBinaryString(byte.toInt() and 0xFF)).replace(' ', '0')
+        }
+
+        // Get the first 8 bits (the prefix) and convert it to hex
+        val prefix = binaryAddress.substring(0, 8).toInt(2).toString(16)
+
+        // Check if the prefix is 0x00, which is the prefix for Polkadot accounts
+        return prefix == "00"
+    }
+    
     init {
         when {
             id.contains("@") -> email = id
             id.lowercase().contains("0x") -> ethAccount = id
             id.lowercase().startsWith("did:") -> did = id
             id.lowercase().startsWith("tz") -> tezosAccount = id
+            isPolkadotAccount(id) -> polkadotAccount = id
         }
     }
 }
