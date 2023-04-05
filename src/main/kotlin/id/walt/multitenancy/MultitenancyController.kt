a3ee17752157d164db89409f79e829b2aa9a3568
waltkb <68587968+waltkb@users.noreply.github.com>
2023-04-05 15:49:41 +0200

    Add tenant unload and tenant deletion functionality

--- src/main/kotlin/id/walt/multitenancy/MultitenancyController.kt
+++ src/main/kotlin/id/walt/multitenancy/MultitenancyController.kt
@@ -1,14 +1,18 @@
 package id.walt.multitenancy
 
 import id.walt.common.KlaxonWithConverters
-import io.javalin.apibuilder.ApiBuilder.get
-import io.javalin.apibuilder.ApiBuilder.path
+import io.javalin.apibuilder.ApiBuilder.*
 import io.javalin.http.ContentType
 import io.javalin.http.Context
 import io.javalin.plugin.openapi.dsl.document
 import io.javalin.plugin.openapi.dsl.documented
+import kotlinx.serialization.Serializable
 
 object MultitenancyController {
+
+    @Serializable
+    data class MultitenancyResult(val success: Boolean)
+
     val routes
         get() = path("multitenancy") {
             get(
@@ -36,6 +40,30 @@ object MultitenancyController {
                     MultitenancyController::listAllTenantIdsByType
                 )
             )
+
+            delete(
+                "unloadTenant/{TenantType}/{TenantId}", documented(
+                    document().operation {
+                        it.summary("Unload a *currently loaded* tenant. Available TenantTypes: ${TenantType.values().map { it.name }}")
+                            .addTagsItem("Multitenancy")
+                            .operationId("unloadTenant")
+                    }.json<MultitenancyResult>("200"),
+                    MultitenancyController::unloadTenant
+                )
+            )
+
+            delete(
+                "deleteTenant/{TenantType}/{TenantId}", documented(
+                    document().operation {
+                        it.summary("Delete a tenant. Available TenantTypes: ${TenantType.values().map { it.name }}")
+                            .addTagsItem("Multitenancy")
+                            .operationId("deleteTenant")
+                    }.json<MultitenancyResult>("200"),
+                    MultitenancyController::deleteTenant
+                )
+            )
+
+
         }
 
     private fun listLoadedTenants(ctx: Context) {
@@ -48,5 +76,23 @@ object MultitenancyController {
     private fun listAllTenantIdsByType(ctx: Context) {
         val tenantType = ctx.pathParam("TenantType")
         ctx.json(TenantContextManager.listAllContextIdsByType(TenantType.valueOf(tenantType)))
+    }
+
+    private fun unloadTenant(ctx: Context) {
+        val type = ctx.pathParam("TenantType")
+        val id = ctx.pathParam("TenantId")
+
+        val res = TenantContextManager.unloadContext(type, id)
+
+        ctx.json(MultitenancyResult(res))
+    }
+
+    private fun deleteTenant(ctx: Context) {
+        val type = ctx.pathParam("TenantType")
+        val id = ctx.pathParam("TenantId")
+
+        val res = TenantContextManager.deleteContext(type, id)
+
+        ctx.json(MultitenancyResult(res))
     }
 }
