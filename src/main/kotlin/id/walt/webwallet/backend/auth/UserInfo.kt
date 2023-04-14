3c74f13e53808ddff4b114b47523fa6c2b1e08ad
ahmed smiai <ahmedsmiai3@gmail.com>
2023-04-15 00:12:47 +0100

    modidfied user info to select the chain based on a prefix

--- src/main/kotlin/id/walt/webwallet/backend/auth/UserInfo.kt
+++ src/main/kotlin/id/walt/webwallet/backend/auth/UserInfo.kt
@@ -3,22 +3,32 @@ package id.walt.webwallet.backend.auth
 import kotlinx.serialization.Serializable
 
 @Serializable
-class UserInfo(
-    val id: String
-) {
+class UserInfo(var id: String ) {
     var email: String? = null
     var password: String? = null
     var token: String? = null
     var ethAccount: String? = null
     var did: String? = null
     var tezosAccount: String? = null
+    var polkadotEvmAccount: String? = null
 
     init {
         when {
             id.contains("@") -> email = id
-            id.lowercase().contains("0x") -> ethAccount = id
-            id.lowercase().startsWith("did:") -> did = id
-            id.lowercase().startsWith("tz") -> tezosAccount = id
+            id.startsWith("did") -> {
+                did = id
+            }
+            id.split("##")[0].lowercase().startsWith("eth") -> {
+                ethAccount = id.split("##")[1]
+                }
+            id.split("##")[0].lowercase().startsWith("tezos") -> {
+                tezosAccount = id.split("##")[1]
+        }
+            id.split("##")[0].lowercase().startsWith("polevm") -> {
+                polkadotEvmAccount = id.split("##")[1]}
+
+            id.lowercase().startsWith("0x") -> {polkadotEvmAccount=id}
+
         }
     }
 }
