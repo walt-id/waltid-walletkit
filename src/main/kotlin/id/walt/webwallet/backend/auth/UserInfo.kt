584b6570b15dc8fbab2433c1bf5bb9a02b136fd9
aminbenmansour <amin.benmansour.10@gmail.com>
2023-05-05 16:19:34 +0100

    add support for flow and generic polkadot addresses

--- src/main/kotlin/id/walt/webwallet/backend/auth/UserInfo.kt
+++ src/main/kotlin/id/walt/webwallet/backend/auth/UserInfo.kt
@@ -10,7 +10,9 @@ class UserInfo(var id: String ) {
     var ethAccount: String? = null
     var did: String? = null
     var tezosAccount: String? = null
+    var polkadotAccount: String? = null
     var polkadotEvmAccount: String? = null
+    var flowAccount: String? = null
 
     init {
         when {
@@ -19,7 +21,9 @@ class UserInfo(var id: String ) {
                 did = id
             }
             id.split("##")[0].lowercase().startsWith("eth") -> {ethAccount = id.split("##")[1] }
+            id.split("##")[0].lowercase().startsWith("pol") -> {polkadotAccount = id.split("##")[1]}
             id.split("##")[0].lowercase().startsWith("polevm") -> {polkadotEvmAccount = id.split("##")[1]}
+            id.split("##")[0].lowercase().startsWith("flow") -> {flowAccount = id.split("##")[1]}
             id.lowercase().startsWith("tz") -> tezosAccount = id
 
         }
