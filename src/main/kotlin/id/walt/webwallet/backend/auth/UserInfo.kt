cd5a080a900865f7bc832b0fa58f0e6f8bcd566e
aminbenmansour <amin.benmansour.10@gmail.com>
2023-05-08 14:59:21 +0100

    refactor: Add prefix to determine account's ecosystem

--- src/main/kotlin/id/walt/webwallet/backend/auth/UserInfo.kt
+++ src/main/kotlin/id/walt/webwallet/backend/auth/UserInfo.kt
@@ -1,24 +1,31 @@
 package id.walt.webwallet.backend.auth
-
 import kotlinx.serialization.Serializable
 
 @Serializable
-class UserInfo(
-    val id: String
-) {
+class UserInfo(var id: String ) {
     var email: String? = null
     var password: String? = null
     var token: String? = null
     var ethAccount: String? = null
     var did: String? = null
     var tezosAccount: String? = null
+    var polkadotAccount: String? = null
+    var polkadotEvmAccount: String? = null
+    var flowAccount: String? = null
 
     init {
         when {
             id.contains("@") -> email = id
-            id.lowercase().contains("0x") -> ethAccount = id
-            id.lowercase().startsWith("did:") -> did = id
+            id.startsWith("did") -> {
+                did = id
+            }
+            id.split("##")[0].lowercase().startsWith("eth") -> {ethAccount = id.split("##")[1] }
+            id.split("##")[0].lowercase().startsWith("pol") -> {polkadotAccount = id.split("##")[1]}
+            id.split("##")[0].lowercase().startsWith("polevm") -> {polkadotEvmAccount = id.split("##")[1]}
+            id.split("##")[0].lowercase().startsWith("flow") -> {flowAccount = id.split("##")[1]}
             id.lowercase().startsWith("tz") -> tezosAccount = id
+
         }
+
     }
 }
