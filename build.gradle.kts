2efcbc5b76cdccbded172cd92e2efb3ef6220837
mikeplotean <mike@walt.id>
2023-05-12 20:58:39 +0300

    chore: howest verifier setup

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,7 +21,7 @@ repositories {
 
 dependencies {
     // SSIKIT
-    implementation("id.walt:waltid-ssikit:1.2305121240.0")
+    implementation("id.walt:waltid-ssikit:1.2305121558.0")
 
     implementation("io.javalin:javalin-bundle:4.6.8")
     implementation("com.github.kmehrunes:javalin-jwt:0.3")
